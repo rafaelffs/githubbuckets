072aaf192bd8751ab3217fa5654588db733c38d3
Gabriel Schade <gabriel.schade@hotmail.com>
2020-11-04 21:09:42 +0000

    Fetch categories and files

---
+++ GitHubBuckets/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubBuckets
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public string download_url { get; set; }
+
+        
+        public string FormattedName { get => Name.Replace(".txt", ""); }
+    }
+}
--- GitHubBuckets/Program.cs
+++ GitHubBuckets/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -10,15 +12,35 @@ namespace GitHubBuckets
         private static readonly HttpClient client = new HttpClient();
         static async Task Main(string[] args)
         {
+            string user = "gabrielschade";
+            string repository = "githubbuckets";
+            string bucketPath = "/bucket";
+            string branch = "master";
+            string category_url = $"https://api.github.com/repos/{user}/{repository}/contents{bucketPath}?ref={branch}";
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            
+            string categoriesRawString = await client.GetStringAsync(category_url);
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(categoriesRawString);
 
-            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            Console.WriteLine("######### Categorias #########");
+            categories.ForEach(category => Console.WriteLine(category.FormattedName));
 
-            var msg = await stringTask;
-            Console.Write(msg);
+            foreach (Category category in categories)
+            {
+                Console.WriteLine($"######### {category.FormattedName} #########");
+                string content = await client.GetStringAsync(category.download_url);
+                string[] items = content.Split("\n");
+                foreach (string item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            
+
+            Console.ReadKey();
         }
     }
 }
